ec124ee5098febbfce74d01e7788e8ab6f317ad5
ErichGiusseppe <eg.soto@uniandes.edu.co>
2025-07-03 21:35:20 -0500

    palindrome

---
+++ PalindromeNumber.py
@@ -0,0 +1,8 @@
+from math import ceil
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        str_int = str(x)
+        for i in range(ceil(len(str_int)/2)): 
+            if str_int[i]!=str_int[-(i+1)]:
+                return False 
+        return True
